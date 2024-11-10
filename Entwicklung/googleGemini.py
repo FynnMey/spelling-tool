e4bfe6d17c0564b8751774ae8cc8a5955491e492
minkaHD <66963029+minkaHD@users.noreply.github.com>
2024-11-10 16:12:41 +0100

    initial

---
+++ Entwicklung/googleGemini.py
@@ -0,0 +1,11 @@
+import google.generativeai as genai
+
+apiPrivateKey = 'AIzaSyB-NySLmTA3tXcThrO-8lHt4leUb6NpKfM'
+
+genai.configure(api_key=apiPrivateKey)
+
+model = genai.GenerativeModel("gemini-1.5-flash")
+response = model.generate_content(
+    "Behebe rechtschreibfehler in Folgendem Text: Hall o ich bin Fynn und ich mahe rechtschrfeifehler")
+print(response.text)
+print(response.text)
